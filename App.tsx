d13e93e4ae5f4a6d76f5c50433d272b267d28106
Barack Mukelenga <barackmukelenga100@gmail.com>
2023-05-16 15:56:03 +0200

    Add support for expo router

--- App.tsx
+++ App.tsx
@@ -1,9 +1,32 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
+import React, { useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+const App: React.FC = () => {
+
+  const [fontsLoaded] = useFonts({
+    'SFMono-Regular': require('./assets/fonts/SFMono-Regular.otf'),
+    'SF-Pro-Rounded': require('./assets/fonts/SF-Pro-Rounded-Medium.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -18,3 +41,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
